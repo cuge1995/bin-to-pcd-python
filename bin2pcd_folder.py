e1d5b781b43c59c7287a237494b4c789f08b6717
Jinlai Zhang <48620704+cuge1995@users.noreply.github.com>
2020-11-10 21:39:47 +0800

    add folder files

---
+++ bin2pcd_folder.py
@@ -0,0 +1,35 @@
+import numpy as np
+import struct
+import os
+import sys
+import open3d as o3d
+
+
+def bin_to_pcd(binFileName):
+    size_float = 4
+    list_pcd = []
+    with open(binFileName, "rb") as f:
+        byte = f.read(size_float * 4)
+        while byte:
+            x, y, z, intensity = struct.unpack("ffff", byte)
+            list_pcd.append([x, y, z])
+            byte = f.read(size_float * 4)
+    np_pcd = np.asarray(list_pcd)
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(np_pcd)
+    return pcd
+
+def main(binFolderName, pcdFolderName):
+    for i in os.listdir(binFolderName):
+        binFileName = binFolderName+i
+        print(i)
+
+        pcd = bin_to_pcd(binFileName)
+        pcdFileName = pcdFolderName+i[:-4]+'.pcd'
+        print(pcdFileName)
+        o3d.io.write_point_cloud(pcdFileName, pcd)
+
+if __name__ == "__main__":
+    a = sys.argv[1]
+    b = sys.argv[2]
+    main(a, b)
